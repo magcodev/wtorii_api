9e069cf51fa071c861b5aa7b59e4f788abd4372f
Michael <mag.codev@gmail.com>
2023-09-17 13:09:43 -0500

    Agregando partes

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvFssHwDuYct6Qds-GZlG6HQKigXDbatI",
+  authDomain: "wtorii.firebaseapp.com",
+  projectId: "wtorii",
+  storageBucket: "wtorii.appspot.com",
+  messagingSenderId: "1042282961776",
+  appId: "1:1042282961776:web:acdc2be559e74f7afd8db5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
